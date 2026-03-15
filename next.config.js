4d9183a34dd3beb86cbc41591656a7b85825b04a
Jamie <jamie@Jamieui-Macmini.local>
2026-03-15 21:10:12 +0900

    chore: disable next image optimization for static export (unoptimized: true)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  // When exporting a static site, disable Next.js image optimization to avoid build-time image processing errors
+  images: {
+    unoptimized: true
+  }
 };
 module.exports = nextConfig;
